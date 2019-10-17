2e2a922d0c8fc4e80df858540e06eecf8c70aa36
Mukul Palit <SLS\mukul.p@l0561l.sls.ads.valuelabs.net>
2019-10-17 17:20:52 +0530

    Golang code to fetch data from API

--- consume_api/main.go
+++ consume_api/main.go
@@ -7,31 +7,29 @@ import(
 	"encoding/json"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
-
 type Response struct {
-    Records []Records `json:"records"`
+    Records []Record `json:"records"`
 }
-
-// A struct to map our Pokemon's Species which includes it's name
-type Records struct {
+// A struct to map the data fetched from the API
+type Record struct {
 	ID string `json:"id"`
-	Country string `"json:country"`
-	State string `"json:state"`
-	City string `"json:city"`
-	Station string `"json:station"`
-	LastUpdate string `"json:last_update"`
-	Pollutant_id string `"json:pollutant_id"`
-	Pollutant_min string `"json:pollutant_min"`
-	Pollutant_max string `"json:pollutant_max"`
-	Pollutant_avg string `"json:pollutant_avg"`
-	Pollutant_unit string `"json:pollutant_unit"`
+	Country string `json:"country"`
+	State string `json:"state"`
+	City string `json:"city"`
+	Station string `json:"station"`
+	LastUpdate string `json:"last_update"`
+	Pollutant_id string `json:"pollutant_id"`
+	Pollutant_min string `json:"pollutant_min"`
+	Pollutant_max string `json:"pollutant_max"`
+	Pollutant_avg string `json:"pollutant_avg"`
+	Pollutant_unit string `json:"pollutant_unit"`
 }
 
-var responseObject Response
-
+var records Response
 func main (){
-	for i := 0; i<=1000; i=i+10 {
+	for i := 510; i<=700; i=i+10 {
 		result := fmt.Sprintf("https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&offset=%d&limit=10&format=json",i)
 		response, err := http.Get(result)
 
@@ -44,28 +42,37 @@ func main (){
 		if err != nil {
 			log.Fatal(err)
 		}
-		//fmt.Println(string(responseData))
-		json.Unmarshal(responseData, &responseObject)
-	}
-	//fmt.Println(responseObject.Name)
-	//fmt.Println(len(responseObject.Pokemon))
+		json.Unmarshal(responseData, &records)
+		//time delay between consecutive fetch request		
+		time.Sleep(700 * time.Millisecond)
 	
-	db, err := sql.Open("mysql", "USERNAME:PASSWORD@tcp(IP_ADDRESS OR LOCALHOST:3306)/DATABASE_NAME")   
+	db, err := sql.Open("mysql", "root:root987@tcp(127.0.0.1:3306)/gov")   
     if err != nil {
         panic(err.Error())
 	}
 
-	defer db.Close()
-	insert, err := db.Prepare("Insert into TABLE_NAME values (?,?,?,?,?,?,?,?,?,?,?)")
+	defer db.Close() 
+	var query string =  "Insert into pollution(RefId, Country, State, City, Station, LastUpdate, Pollution_ID, Pollution_min, Pollution_max, Pollution_avg, Pollution_unit) values (?,?,?,?,?,?,?,?,?,?,?)"
+	insert, err := db.Prepare(query)
 	if err != nil {
         panic(err.Error())
 	}
-	for i := 0; i < len(responseObject.Records); i++ {
-		fmt.Println(responseObject.Records[i].ID)
-		fmt.Println(responseObject.Records[i].Country)
-		fmt.Println(responseObject.Records[i].State)
-		//insert.Exec(responseObject.Records[i].ID,)
+	for i := 0; i < len(records.Records); i++ {
+		insert.Exec(
+			records.Records[i].ID,
+			records.Records[i].Country, 
+			records.Records[i].State, 
+			records.Records[i].City, 
+			records.Records[i].Station, 
+			records.Records[i].LastUpdate, 
+			records.Records[i].Pollutant_id, 
+			records.Records[i].Pollutant_min, 
+			records.Records[i].Pollutant_max, 
+			records.Records[i].Pollutant_avg, 
+			records.Records[i].Pollutant_unit,
+		)
 	}
 	defer insert.Close()
 
+}
 }
